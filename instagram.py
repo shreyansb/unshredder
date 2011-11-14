f3ba86305e756563b2bf0cc8a3acc8c6740ed47e
Shreyans Bhansali <shreyans@venmo.com>
2011-11-14 00:57:32 -0500

    keeping track of experiments

--- instagram.py
+++ instagram.py
@@ -4,14 +4,12 @@ written on Sunday, the 13th of November, 2011
 """
 
 from PIL import Image
-import random
-import time
 from itertools import islice, count
 
 class Unshredder():
-    pixel_diff_threshold = 35       # consider two pixel attrs a match if their diff 
+    pixel_diff_threshold = 25       # consider two pixel attrs a match if their diff 
                                     # is less than this number
-    column_match_threshold = 0.45   # consider two columns a match if more than
+    column_match_threshold = 0.60   # consider two columns a match if more than
                                     # this portion of their pixels match
 
     def __init__(self, image_file='TokyoPanoramaShredded.png', strip_width=32):
@@ -25,15 +23,14 @@ class Unshredder():
         # create a list of empty lists, with room for the left and right most
         # pixels for each strip
         self.strip_edges = [[] for i in range(self.number_of_strips*2)]
-        #self.sort_order = range(self.number_of_strips)
-        # start it off with a random strip
-        self.sort_order = [random.randint(0, self.number_of_strips-1)]
-        #self.sort_order = [0]
+        self.sort_order = [19]
+        self.comparisons = 0
 
     def unshred(self):
         self.set_strip_edges()
         self.sort_strips()
         self.create_image_from_sort_order()
+        print self.comparisons, "comparisons"
 
     def set_strip_edges(self):
         """Collects columns of pixels that represent the left and right
@@ -57,10 +54,13 @@ class Unshredder():
         """
         starting_order = list(self.sort_order)
         l_strip, r_strip = self.get_neighbours()
-        if l_strip:
+        print "left and right", l_strip, r_strip
+        if l_strip != None:
             self.sort_order = [l_strip] + self.sort_order
-        if r_strip:
+            print "adding %s to the left" % l_strip
+        if r_strip != None:
             self.sort_order = self.sort_order + [r_strip]
+            print "adding %s to the right" % r_strip
         if len(self.sort_order) < self.number_of_strips:
             print self.sort_order
             if starting_order == self.sort_order:
@@ -77,13 +77,13 @@ class Unshredder():
         for i in range(self.number_of_strips):
             if i in self.sort_order: continue
             left = self.compare_strips(l_most_strip, i, find_left=True)
-            if left != 0:
+            if left != None:
                 left = i
                 break
         for i in range(self.number_of_strips):
             if i in self.sort_order: continue
             right = self.compare_strips(r_most_strip, i, find_right=True)
-            if right != 0:
+            if right != None:
                 right = i
                 break
         return left, right
@@ -96,6 +96,7 @@ class Unshredder():
         """
         # compare s1 right with s2 left
         if find_right:
+            self.comparisons += 1
             s1r = self.strip_edges[(2 * s1) + 1]
             s2l = self.strip_edges[2 * s2]
             diff1 = self.compare_columns(s1r, s2l)
@@ -104,23 +105,35 @@ class Unshredder():
 
         # compare s1 left with s2 right
         if find_left:
+            self.comparisons += 1
             s1l = self.strip_edges[2 * s1]
             s2r = self.strip_edges[(2 * s2) + 1]
             diff2 = self.compare_columns(s1l, s2r)
             if diff2:
                 return -1
 
-        return 0
+        return None
 
     def compare_columns(self, col1, col2):
         """Compare two columns of pixels.
         Returns 1 is they are deemed neighbours, 0 otherwise.
         """
-        total_matches = 0.0
+        total_matches_top = 0.0
+        total_matches_bottom = 0.0
+        divider = int(self.height / 2)
         for i in xrange(self.height):
-            total_matches += self.compare_pixels(col1[i], col2[i])
+            pixel_match = self.compare_pixels(col1[i], col2[i])
+            if i < divider:
+                total_matches_top += pixel_match
+            else:
+                total_matches_bottom += pixel_match
         #print total_matches, self.height, round((total_matches / self.height),2)
-        if (total_matches / self.height) > self.column_match_threshold:
+        print "top match score: %s" % (total_matches_top / self.height*2)
+        print "bottom match score: %s" % (total_matches_bottom / self.height*2)
+        if ((total_matches_top / self.height*2) > self.column_match_threshold or
+            (total_matches_bottom / self.height*2) > self.column_match_threshold):
+            #print "match score: %s" % (total_matches / self.height)
+            print "returning match"
             return 1
         return 0
 
@@ -128,21 +141,25 @@ class Unshredder():
         """Compare two pixels.
         Returns 1 if they are deemed neighbours, 0 otherwise
         """
-        for i in range(4):
-            if abs(p1[i] - p2[i]) > self.pixel_diff_threshold:
-                return 0
+        total_diff = 0.0
+        for i in range(3):
+            total_diff += (abs(p1[i] - p2[i]))
+        if (total_diff/3) > self.pixel_diff_threshold:
+            return 0 
+            #if abs(p1[i] - p2[i]) > self.pixel_diff_threshold:
+            #    return 0
         return 1
 
     def create_image_from_sort_order(self):
         """Creates a new image based on the order of strips in self.sort_order
         """
         unshredded = Image.new("RGBA", self.image.size)
-        filename = "unshredded-%s.png" % int(time.time())
+        new_image_name = "unshredded.png"
         for i, strip_no in enumerate(self.sort_order):
             strip = self.get_strip(strip_no)
             destination_point_top_left = (i * self.strip_width, 0)
             unshredded.paste(strip, destination_point_top_left)
-            unshredded.save(filename, "PNG")
+            unshredded.save(new_image_name, "PNG")
 
     def get_strip(self, strip_no):
         """Returns an ImageCrop object for the strip
